51a1b07056721da599dfb065747ec49464354915
Mitsuki Ogasahara <negiga@gmail.com>
2024-07-19 16:14:27 +0900

    Remove dev server from bin

--- bin/url-press.js
+++ bin/url-press.js
@@ -1,31 +1,13 @@
-import { createRequestHandler } from "@remix-run/express";
-import express from "express";
+import { createRequestHandler } from "@remix-run/express"
+import express from "express"
 
-const viteDevServer =
-  process.env.NODE_ENV === "production"
-    ? null
-    : await import("vite").then((vite) =>
-        vite.createServer({
-          server: { middlewareMode: true },
-        })
-      );
+const app = express()
+app.use(express.static("build/client"))
 
-const app = express();
-app.use(
-  viteDevServer
-    ? viteDevServer.middlewares
-    : express.static("build/client")
-);
+const build = await import("./build/server/index.js")
 
-const build = viteDevServer
-  ? () =>
-      viteDevServer.ssrLoadModule(
-        "virtual:remix/server-build"
-      )
-  : await import("./build/server/index.js");
-
-app.all("*", createRequestHandler({ build }));
+app.all("*", createRequestHandler({ build }))
 
 app.listen(3000, () => {
-  console.log("App listening on http://localhost:3000");
-});
+  console.log("App listening on http://localhost:3000")
+})
