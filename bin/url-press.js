4077e8611a9d2efb578a21eb531995e7ebd22e83
Mitsuki Ogasahara <negiga@gmail.com>
2024-07-19 16:44:59 +0900

    listen PORT environment

--- bin/url-press.js
+++ bin/url-press.js
@@ -9,6 +9,7 @@ const build = await import("../build/server/index.js")
 
 app.all("*", createRequestHandler({ build }))
 
-app.listen(3000, () => {
-  console.log("App listening on http://localhost:3000")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`)
 })
